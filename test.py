a61887af9760297c6bfabd8d4f43d410e3f7946d
César Pedraza <cpedraza@unal.edu.co>
2021-03-26 21:14:08 -0500

    add tests

---
+++ test.py
@@ -0,0 +1,44 @@
+from numpy import median
+from numpy.random import randint
+from sort import mergeSort
+from time import time
+
+
+def singleSortTest(n, sortingAlgorithm):
+    arr = randint(1, 5000 + 1, n)
+    start = time()
+    sortingAlgorithm(arr)
+    end = time()
+
+    return end - start
+
+def sortTest(n, sortingAlgorithm, timeLimit, sorts):
+    results = []
+    for _ in range(sorts):
+        results.append(singleSortTest(n, sortingAlgorithm))
+        if (results[-1] > timeLimit):
+            results.pop()
+            break
+    
+    return results
+
+def test(sortingAlgorithm,
+         dn = 1,
+         singleTestLimit = 5*60,
+         sortsPerTest = 100):
+    n, results = dn, []
+    results = []
+    while True:
+        result = sortTest(n,
+                          sortingAlgorithm,
+                          singleTestLimit,
+                          sortsPerTest
+            )
+        
+        if len(result) < sortsPerTest:
+            break
+        results.append(median(result))
+        print("n =", n)
+        n += dn
+    
+    return results
