725eb5f982ed2fa3235675c4f2eaaa2117be011a
César Pedraza <cpedraza@unal.edu.co>
2021-05-03 19:52:08 -0500

    add heapsort

--- sort.py
+++ sort.py
@@ -63,3 +63,27 @@ def countingSort(arr, leftLimit, rightLimit):
     #for i in range(len(arr)):
     #    arr[i] = new[i]
     arr[:] = new
+
+# heap-sort and its subroutines
+def siftDown(arr, length, i):
+    maxIndex = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < length and not arr[left] <= arr[maxIndex]:
+        maxIndex = left
+    if right < length and not arr[right] <= arr[maxIndex]:
+        maxIndex = right
+
+    if i != maxIndex:
+        arr[i], arr[maxIndex] = arr[maxIndex], arr[i]
+        siftDown(arr, length, maxIndex)
+
+
+def heapSort(arr):
+    for i in range(len(arr) // 2 - 1, -1, -1):
+        siftDown(arr, len(arr), i)
+
+    for i in range(len(arr) - 1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        siftDown(arr, i, 0)
