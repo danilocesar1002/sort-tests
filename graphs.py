160c3afca78279a8e66b9230cee5c9a56296377d
César Pedraza <cpedraza@unal.edu.co>
2021-05-10 18:15:01 -0500

    add names in the graphs

--- graphs.py
+++ graphs.py
@@ -5,25 +5,35 @@ import numpy as np
 def movingAverage(x, w):
     return np.convolve(x, np.ones(w), 'valid') / w
 
-def makePlot(test, ax):
+def makePlot(test, name, ax):
     movAvg = movingAverage(test[1], len(test[1])//100 + 1)
-    ax.plot(range(1,len(movAvg) + 1), movAvg)
-    ax.scatter(test[0], test[1],s=0.5,alpha=0.3)
+    ax.plot(range(len(test[1]) - len(movAvg), len(test[1])), movAvg)
+    ax.scatter(test[0], test[1], s=0.5, alpha=0.3, label=name)
 
 
 def main():
     tests = open("test.txt", "r").read().splitlines()
     tests = [eval(test) for test in tests]
     tests = [np.transpose(test) for test in tests]
+    names = [
+        "insertion-sort",
+        "merge-sort",
+        "heap-sort",
+        "counting-sort",
+        "quick-sort"
+    ]
+
+    assert len(names) == len(tests)
 
     fig = plt.figure()
     ax = fig.add_subplot()
     ax.set_xlabel("n")
     ax.set_ylabel("T(n)")
     
-    for test in tests:
-        makePlot(test, ax)
-    
+    for i in range(len(tests)):
+        makePlot(tests[i], names[i], ax)
+
+    plt.legend()
     plt.show()
 
 
