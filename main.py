04d0a24bf64438cbc6becf694e8adedaee453ca4
César Pedraza <cpedraza@unal.edu.co>
2021-05-13 07:30:52 -0500

    almost nothing

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
 
     print("Este es un script de pruebas para algoritmos de ordenamiento.")
     print("Las pruebas pueden tomar varias horas.")
-    print("Los resultados se guardarán en \"{0}\". Si el archivo ya existe,".format(path)
-        + " su contenido será borrado y reemplazado.")
+    print("Los resultados se guardarán en \"{}\". Si el archivo ya existe, ".format(path)
+        + "su contenido será borrado y reemplazado.")
     
     while (inpt := input("¿Desea iniciar con las pruebas? (s/n): ")) != "n":
         if inpt == "s":
